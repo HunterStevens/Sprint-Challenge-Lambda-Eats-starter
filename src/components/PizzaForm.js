ecf32dee50556177e52c12850b0a8183f8e45b8e
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-27 11:41:35 -0600

    completed schema and validation code in form

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,11 +1,64 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import * as yup from 'yup';
 
-
+const orderSchema = yup.object().shape({
+pizzaSize:yup.string().required("Choose your pizza size."),
+orderName:yup.string().min(2, "must put a name of at least two letters.")
+});
 
 function PizzaForm(){
 
 const [submitDisabled, setSubmitDisabled] = useState(true);
+
+const [orderInput, setOrderInput] = useState({
+    pizzaSize:"",
+    toppings:"",
+    special:"",
+    orderName:""
+
+}) 
+
+const [orderErr, setOrderErr] = useState({
+    pizzaSize:"",
+    orderName:""
+})
+
+const [pizzaOrder, setPizzaOrder] = useState([]);
+
+
+useEffect(() => {
+    orderSchema.isValid(orderInput).then(valid =>{
+        setSubmitDisabled(!valid);
+    })
+})
+
+const orderChange = (event) =>{
+    event.persist();
+    const newOrder ={...orderInput};
+
+    validateOrder(event);
+    setOrderInput(newOrder);
+
+}
+
+const validateOrder = (event) => {
+    yup.reach(orderSchema, event.target.name)
+    .validate(event.target.value)
+    .then(good =>{
+        setOrderErr({
+            ...orderErr,
+            [event.target.name]:""
+        });
+    })
+    .catch(err =>{
+        setOrderErr({
+            ...orderErr,
+            [event.target.name]:err.errors[0]
+        });
+    });
+}
 
 const addOrder = (event) => {
     event.preventDefault();
@@ -21,11 +74,14 @@ const addOrder = (event) => {
         <form onSubmit={addOrder} >
             <label htmlFor="PizzaSize">
                 Size for your Pizza: 
-                <select id="PizzaSize" name="PizzaSize">
+                <select id="PizzaSize" name="PizzaSize"
+                value={orderInput.pizzaSize} onChange={orderChange} >
+                    <option value="">--Select Size--</option>
                     <option value="sm">Small</option>
                     <option value="md">Medium</option>
                     <option value="lg">Large</option>
                 </select>
+    {orderErr.pizzaSize.length > 0 ? <p id="sizeErr">{orderErr.pizzaSize}</p> : null }
             </label>
 
             <br/>
@@ -56,14 +112,17 @@ const addOrder = (event) => {
 
             <label htmlFor="special">
                 Special Instructions:
-                <textarea id="special" name="special"/>
+                <textarea id="special" name="special"
+                value={orderInput.orderName} onChange={orderChange}/>
             </label>
 
             <br/>
 
             <label htmlFor="orderName">
                 Name for the Order:
-                <input type="text" name="orderName" id="orderName" />
+                <input type="text" name="orderName" id="orderName" 
+                value={orderInput.orderName} onChange={orderChange}/>
+                {orderErr.orderName.length > 0 ? <p id="sizeErr">{orderErr.orderName}</p> : null }
             </label>
 
             <br/>
